53da5b33910f9d4d5e80c9ffff7f977c2a7de139
Anh Tong <tongkimanh@yahoo.com>
2021-07-24 21:51:54 +0000

    Add file

--- eqArrays.js
+++ eqArrays.js
@@ -18,10 +18,24 @@ const assertEqual = function(actual, expected) {
     console.log(fail1);
   }
 };
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// TEST CODE
-
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(7, "7");
-assertEqual("Javascript", "Javascript");
+// TEST SOLUTIONS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], [2]), false);
