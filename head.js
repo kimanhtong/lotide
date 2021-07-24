58346f62955e97ffb130b6244d00beb4b383673f
Anh Tong <tongkimanh@yahoo.com>
2021-07-24 05:26:43 +0000

    Add head.js file

---
+++ head.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  let act = actual;
+  let exp = expected;
+  if (typeof actual === "string") {
+    act = `"${actual}"`;
+  }
+  if (typeof expected === "string") {
+    exp = `"${expected}"`;
+  }
+  let emojip = String.fromCodePoint(0x1F49A);
+  let emojif = String.fromCodePoint(0x1F975);
+  let pass1 = `${emojip}${emojip}Assertion Passed: ${act} === ${exp}`;
+  let fail1 = `${emojif}${emojif}${emojif}Assertion Failed: ${act} !== ${exp}`;
+  if (act === exp) {
+    console.log(pass1);
+  } else {
+    console.log(fail1);
+  }
+};
+const head = function(arr) {
+  return arr[0];
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([1]), 1);
+assertEqual(head([,3,5]),undefined);
