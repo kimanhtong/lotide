cb1ff2a3d33eac0e857f30bde7fa2ea5cabf62ca
Anh Tong <tongkimanh@yahoo.com>
2021-08-10 20:53:37 +0000

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-const assertArraysEqual = function(act, exp) {
-  let pass1 = `💚💚💚 Assertion Passed: ${act} === ${exp}`;
-  let fail1 = `🥵🥵🥵 Assertion Failed: ${act} !== ${exp}`;
-  if (eqArrays(act, exp)) {
-    console.log(pass1);
-  } else {
-    console.log(fail1);
-  }
-};
 const middle = function (arr) {
   const arr1 = [];
   const l = arr.length;
@@ -34,10 +13,4 @@ const middle = function (arr) {
   return arr1;
 };
 
-// Test the function middle.js
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
